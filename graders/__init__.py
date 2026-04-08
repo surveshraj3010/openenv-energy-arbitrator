39d1ba5a71dec3b0e522bcd0c0b0258df7b9824a
surveshraj3010 <78865866+surveshraj3010@users.noreply.github.com>
2026-04-08 20:55:47 +0530

    Delete graders directory

--- graders/__init__.py
+++
@@ -1,3 +0,0 @@
-from graders.graders import EasyGrader, MediumGrader, HardGrader, get_grader, GRADER_REGISTRY
-
-__all__ = ["EasyGrader", "MediumGrader", "HardGrader", "get_grader", "GRADER_REGISTRY"]
